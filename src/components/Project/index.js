5e8e1850aefbc4c5f20248d2a731aaa52263c178
sadon26 <ashadayosamuel@gmail.com>
2022-07-09 12:58:12 +0100

    update made

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -6,8 +6,13 @@ const Project = ({ slide, index }) => {
       <div className="h-full">
         <img className="w-full h-full fit-image" src={slide.logo} alt={slide.alt} />
       </div>
-      <div className="project__description absolute">
-        <h1 className="fs-43 fw-600">{slide.title}</h1>
+      <div className="project__heading w-full absolute">
+        <p className="fs-17 mb-2 display-block project__heading-title fw-600">PROJECT {index + 1}</p>
+        <div className="project__heading-separator" />
+      </div>
+      <div className="project__description absolute w-60">
+        <h1 className="fs-43 fw-600 mb-2">{slide.title}</h1>
+        <p className="fs-14 mb-4">{slide.description}</p>
         <a className="fs-12" href={slide.nextPage} target="_blank" rel="noreferrer">
           VISIT SITE
         </a>
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -3,9 +3,10 @@ import { Project } from 'components';
 import InfohobLanding from 'assets/images/infohob-landing.jpg';
 import KafeneLanding from 'assets/images/kafene-landing.jpg';
 import InventoryLanding from 'assets/images/inventory-landing.jpg';
+import IndigoLanding from 'assets/images/indigo-landing.jpg';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Mousewheel, EffectCoverflow } from 'swiper';
-import gsap, { Back } from 'gsap';
+import gsap, { Back, Elastic } from 'gsap';
 import './_project.scss';
 
 const Projects = () => {
@@ -14,21 +15,39 @@ const Projects = () => {
       logo: InfohobLanding,
       alt: 'infohob-landing',
       title: 'Infohob',
+      description: `A job portal application focused on techies which helps ease the stress of recruitment by bringing the
+        job seeker and recruiter to the same platform.
+      `,
       nextPage: 'https://infohob.com',
     },
     {
       logo: KafeneLanding,
       alt: 'kafene-landing',
       title: 'Kafene',
+      description: `Customized lease application which add flexibility and affordability
+         to your life and helps you get the products you want without having to pay for it all upfront.`,
       nextPage: 'https://kafene.com',
     },
     {
       logo: InventoryLanding,
       alt: 'inventory-landing',
       title: 'Inventory',
+      description: `An Inventory Management System(IMS) which helps companies keep track of their warehouses, products, distributors, 
+        vendors, sales and business performance.
+      `,
       nextPage: 'https://staginginventory.enyata.com',
     },
+    {
+      logo: IndigoLanding,
+      alt: 'indigo-landing',
+      title: 'Indigo',
+      description: `A medical software which connects all the processes between the patients and staffs from different hospitals 
+        into a single, simple and easy to operate platform.
+      `,
+      nextPage: 'https://app.indigoemr.com/login',
+    },
   ];
+
   const animateElements = (slide) => {
     gsap.fromTo(
       `.swiper-${slide.realIndex + 1} img`,
@@ -39,6 +58,32 @@ const Projects = () => {
         scale: 1.2,
         duration: 1,
         delay: 1,
+      },
+    );
+    gsap.fromTo(
+      `.swiper-${slide.realIndex + 1} .project__heading-title`,
+      {
+        autoAlpha: 0,
+        y: 20,
+      },
+      {
+        y: 0,
+        autoAlpha: 1,
+        duration: 1,
+        delay: 1,
+        ease: Back,
+      },
+    );
+    gsap.fromTo(
+      `.swiper-${slide.realIndex + 1} .project__heading-separator`,
+      {
+        width: 0,
+      },
+      {
+        delay: 1,
+        duration: 2,
+        width: '100%',
+        ease: Elastic,
       },
     );
     gsap.fromTo(
