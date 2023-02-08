579424eeeef9eed7b7c396711b453610583cce3d
sadon26 <ashadayosamuel@gmail.com>
2023-02-08 08:05:27 +0100

    updated projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,54 +1,13 @@
 import React, { useState } from 'react';
 import { Project } from 'components';
-import InfohobLanding from 'assets/images/infohob-landing.jpg';
-import KafeneLanding from 'assets/images/kafene-landing.jpg';
-import InventoryLanding from 'assets/images/inventory-landing.jpg';
-import IndigoLanding from 'assets/images/indigo-landing.jpg';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Mousewheel, EffectCoverflow } from 'swiper';
 import { animateProjectContents } from 'utils/gsapAnimations';
+import slides from 'helpers/slides';
 import './_project.scss';
 
 const Projects = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
-
-  const slides = [
-    {
-      logo: InfohobLanding,
-      alt: 'infohob-landing',
-      title: 'Infohob',
-      description: `A job portal application focused on techies which helps ease the stress of recruitment by bringing the
-        job seeker and recruiter to the same platform.
-      `,
-      nextPage: 'https://infohob.com',
-    },
-    {
-      logo: KafeneLanding,
-      alt: 'kafene-landing',
-      title: 'Kafene',
-      description: `Customized lease application which add flexibility and affordability
-         to your life by helping you get the products you want without having to pay for it all upfront.`,
-      nextPage: 'https://kafene.com',
-    },
-    {
-      logo: InventoryLanding,
-      alt: 'inventory-landing',
-      title: 'Inventory',
-      description: `An Inventory Management System(IMS) which helps companies keep track of their warehouses, products, distributors, 
-        vendors, sales and business performance.
-      `,
-      nextPage: 'https://staginginventory.enyata.com',
-    },
-    {
-      logo: IndigoLanding,
-      alt: 'indigo-landing',
-      title: 'Indigo',
-      description: `A medical software which connects all the processes between the patients and staffs from different hospitals 
-        into a single, simple and easy to operate platform.
-      `,
-      nextPage: 'https://app.indigoemr.com/login',
-    },
-  ];
 
   return (
     <div className="h-screen relative">
